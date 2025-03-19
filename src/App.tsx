ecc9a7733dc6284df08251baf450c7a54af723d4
Kartikscript <kartik.13102003@gmail.com>
2025-03-19 19:35:54 +0530

    insta home layout completed in 5 hrs

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+import { useState } from 'react'
+import { Compass, Film, Heart, House, Menu, Search, Send, SquarePlus } from 'lucide-react'
+import PostCard from './components/PostCard'
+
+
+const navLinks = [
+  {
+    id:'1',
+    title:'Home',
+    icon:<House/>
+  },
+  {
+    id:'2',
+    title:'Search',
+    icon:<Search/>
+  },
+  {
+    id:'3',
+    title:'Explore',
+    icon:<Compass/>
+  },
+  {
+    id:'4',
+    title:'Reels',
+    icon:<Film/>
+  },
+  {
+    id:'5',
+    title:'Messages',
+    icon:<Send className='rotate-12'/>
+  },
+  {
+    id:'6',
+    title:'Notifications',
+    icon:<Heart/>
+  },
+  {
+    id:'7',
+    title:'Create',
+    icon:<SquarePlus/>
+  },
+  {
+    id:'8',
+    title:'Profile',
+    icon:(
+        <img
+          src='/insta-profile.jpg'
+          alt='user'
+          className='rounded-full object-cover overflow-hidden'
+        />
+    )
+  },
+  
+]
+function App() {
+  const [activeTabId, setActiveTabId] = useState('1')
+
+  return (
+   <>
+    <main className='h-screen relative overflow-y-scroll flex bg-black text-white'>
+      <div className='h-screen w-60'>
+        <aside className='h-full w-60 fixed top-0 left-0 bg-black flex flex-col justify-between px-3 pt-2 pb-5 border-r border-white/15'>
+          <div>
+            <div className='px-3 pb-4 pt-8 mb-5 w-32'>
+              <img
+                src='/insta-text-logo.png'
+                alt='Instagram'
+              />
+            </div>
+            <ul className='space-y-1.5'>
+              {
+                navLinks.map(({id,icon,title},i)=>{
+                  const isActive = activeTabId === id
+                  return(
+                    <li
+                      key={i}
+                      onClick={()=>setActiveTabId(id)}
+                      className={`p-3 group flex items-center gap-4 cursor-pointer rounded-3xl hover:bg-white/10 ${isActive && 'font-bold **:stroke-3'} active:bg-white/5 transition-all duration-200`}
+                    >
+                      <div className='relative'>
+                        <span className=' *:size-6'>{icon}</span>
+                        {title === 'Messages' && <div className='absolute right-0 top-0 translate-x-1/4 -translate-y-1/4 bg-red-500 border-2 group-hover:border-current border-black size-[18px] flex justify-center items-center rounded-full text-[10px] '>2</div>}
+                        {title === 'Notifications' && <div className='absolute right-0 top-0 translate-x-1/4 bg-red-500 border-2 group-hover:border-current border-black size-3 rounded-full'/>}
+                      </div>
+                      {title}
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+          <div 
+          onClick={()=>setActiveTabId('11')}
+          className={`p-3 flex items-center gap-4 *:size-6 text-white cursor-pointer rounded-3xl hover:bg-white/10 ${(activeTabId === '11') && 'font-bold *:stroke-3'} active:bg-white/5 transition-all duration-200`}
+          >
+            <Menu/>More
+          </div>
+        </aside>
+
+      </div>
+      <div className='flex-1 '>
+        <section className='w-[630px] mx-auto px-20'>
+          <PostCard/>
+          <PostCard/>
+          <PostCard/>
+        </section>
+      </div>
+    </main>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,67 @@
+import { BadgeCheck, Bookmark, Ellipsis, Heart, MessageCircle, Send, Smile, X } from "lucide-react"
+
+const PostCard = () => {
+  return (
+    <main className='w-full space-y-4 pt-6 pb-4 border-b border-white/15'>
+      <div className="flex justify-between items-center ">
+        <p>Suggested for you</p>
+        <X className="opacity-70 size-4"/>
+      </div>
+      <div className="text-sm flex gap-2 w-full">
+          <div className="size-10 rounded-full flex justify-center items-center bg-gradient-to-tr from-amber-600 via-pink-600 to-fuchsia-600 from-30% via-60%">
+            <img src="/insta-profile.jpg" alt="user" className="rounded-full border-2 border-black size-9 object-cover overflow-hidden" />
+          </div>
+          <div>
+            <ul className="flex gap-1">
+              <li className="font-bold flex items-center gap-0.5 ">sam.williams12 <BadgeCheck className="size-4 fill-blue-500 text-black"/></li>
+              <li className="opacity-70">•</li>
+              <li className="text-white/50">1w</li>
+              <li className="opacity-70">•</li>
+              <li className="text-blue-600 font-bold">Follow</li>
+            </ul>
+            <h4>Original Audio</h4>
+          </div>
+          <div className="flex-1 flex justify-end items-center">
+            <Ellipsis/>
+          </div>
+      </div>
+      <div className="w-full overflow-hidden rounded-xl border border-white/15 bg-black flex justify-center ">
+        <img 
+          src="/post.png"
+          alt="post"
+        />
+      </div>
+      <section className="space-y-1.5 text-sm">
+        <div className="flex justify-between  ">
+          <div className="flex gap-4 items-center *:hover:opacity-50 *:transition-all *:cursor-pointer">
+            <Heart/>
+            <MessageCircle/>
+            <Send className="rotate-12"/>
+          </div>
+          <Bookmark className="hover:opacity-50 transition-all cursor-pointer"/>
+        </div>
+        <div className="flex gap-3 ">
+          <div className="*:size-4 *:-mr-2 *:border-1 *:border-black *:object-cover *:rounded-full *:overflow-hidden *:inline-block">
+            <img
+              src="/insta-profile.jpg"
+              alt="user"
+            />
+            <img
+              src="/insta-profile.jpg"
+              alt="user"
+            />
+          </div>
+          <p>Liked by <span className="font-bold">thisizlondon</span> and <span className="font-bold">others</span></p>
+        </div>
+        <p className="flex items-center gap-0.5 "><span className="font-bold ">johny_wiz32</span> <BadgeCheck className="size-4 fill-blue-500 text-black"/> Thankful for inner peace</p>
+        <p className="opacity-60">View all 93 comments</p>
+        <div className="opacity-60 flex justify-between">
+          <p>Add a comment...</p>
+          <Smile className="size-3.5"/>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default PostCard
